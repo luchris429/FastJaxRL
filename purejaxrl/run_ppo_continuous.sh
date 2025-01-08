d613f49da0567cf7fcef0a6547ab069d9227f259
shashank <shshnkreddy@gmail.com>
2025-01-08 14:23:22 +0800

    logging

---
+++ purejaxrl/run_ppo_continuous.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#################################################
+## TEMPLATE VERSION 1.01                       ##
+#################################################
+## ALL SBATCH COMMANDS WILL START WITH #SBATCH ##
+## DO NOT REMOVE THE # SYMBOL                  ## 
+#################################################
+
+#SBATCH --nodes=1                   # How many nodes required? Usually 1
+#SBATCH --cpus-per-task=10         # Number of CPU to request for the job
+#SBATCH --mem=24GB                   # How much memory does your job require?
+#SBATCH --gres=gpu:1                # Do you require GPUS? If not delete this line
+#SBATCH --time=02-00:00:00          # How long to run the job for? Jobs exceed this time will be terminated
+                                    # Format <DD-HH:MM:SS> eg. 5 days 05-00:00:00
+                                    # Format <DD-HH:MM:SS> eg. 24 hours 1-00:00:00 or 24:00:00
+#SBATCH --mail-type=FAIL  # When should you receive an email?
+#SBATCH --output=%u.%j.out          # Where should the log files go?
+                                    # You must provide an absolute path eg /common/home/module/username/
+                                    # If no paths are provided, the output file will be placed in your current working directory
+#SBATCH --requeue                   # If the job is terminated, should it be restarted?
+#SBATCH --prefer=h100nvl
+################################################################
+## EDIT AFTER THIS LINE IF YOU ARE OKAY WITH DEFAULT SETTINGS ##
+################################################################
+
+#SBATCH --partition=researchshort                 # The partition you've been assigned
+#SBATCH --account=pradeepresearch   # The account you've been assigned (normally student)
+#SBATCH --qos=research-1-qos       # What is the QOS assigned to you? Check with myinfo command
+#SBATCH --mail-user=shashankc@smu.edu.sg # Who should receive the email notifications
+#SBATCH --job-name=evolve     # Give the job a name
+
+#################################################
+##            END OF SBATCH COMMANDS           ##
+#################################################
+
+# Purge the environment, load the modules we require.
+# Refer to https://violet.smu.edu.sg/origami/module/ for more information
+module purge
+module load Anaconda3/2024.06-1
+
+# Do not remove this line even if you have executed conda init
+eval "$(conda shell.bash hook)"
+
+# This command assumes that you've already created the environment previously
+# We're using an absolute path here. You may use a relative path, as long as SRUN is execute in the same working directory
+conda activate evolve
+
+# Submit your job to the cluster
+srun --gres=gpu:1 python evolve_ppo_continuous_action.py
